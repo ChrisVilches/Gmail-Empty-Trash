74b0bf469f8e47481c7ccf3570bb83b8de2123a1
Chris Vilches <felovilches@gmail.com>
2022-01-07 02:18:14 +0900

    change port

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def create_messages_client():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=8080)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
